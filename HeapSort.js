b6403df3b0c0d6c180b3ca3c57f6a9210b165d03
dudckd6744 <65056776+dudckd6744@users.noreply.github.com>
2021-12-16 15:59:31 +0900

    분석 필요

---
+++ HeapSort.js
@@ -0,0 +1,45 @@
+let arrLen;
+
+function swap(arr, i, j){
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function heapify(arr, i){
+  let max = i;
+  let left = 2 * i + 1;
+  let right = 2 * i + 2;
+  
+  if(left < arrLen && arr[left] > arr[max]){
+    max = left;
+  }
+  
+  if(right < arrLen && arr[right] > arr[max]){
+    max = right;
+  }
+  
+  if(max != i){
+    swap(arr, i, max);
+    heapify(arr, max);
+  }
+}
+
+function heapSort(arr){
+  arrLen = arr.length;
+
+  for(let i = Math.floor(arrLen / 2); i >= 0; i--){
+    heapify(arr, i);
+  }
+  
+  for(let i = arrLen - 1; i > 0; i--){
+    swap(arr, 0, i);
+    arrLen--;
+    
+    heapify(arr, 0);
+  }
+  
+  return arr;
+}
+
+console.log(heapSort([13, 1, 3, 7, 9, 11, 5]));
