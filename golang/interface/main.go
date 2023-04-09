cf656010f2e90d3ef0063801757a4a36c8197f5d
우영창 <dudckd9650@gmail.com>
2023-04-09 22:29:39 +0900

    golang - 메모리 '

--- golang/interface/main.go
+++ golang/interface/main.go
@@ -19,32 +19,32 @@ type Cylinder struct {
 }
 
 func (r RectangularParallelepiped) area() float64 {
-	return (2*r.line1*r.line2) + (2*r.line1*r.line3) + (2*r.line2*r.line3)
+	return (2 * r.line1 * r.line2) + (2 * r.line1 * r.line3) + (2 * r.line2 * r.line3)
 }
 
 func (r RectangularParallelepiped) volume() float64 {
-	return r.line1*r.line2*r.line3
+	return r.line1 * r.line2 * r.line3
 }
 
 func (c Cylinder) area() float64 {
-	return (math.Pi * c.radius * c.radius) * 2 + (2*math.Pi*c.radius)*c.height
+	return (math.Pi*c.radius*c.radius)*2 + (2*math.Pi*c.radius)*c.height
 }
 func (c Cylinder) volume() float64 {
 	return (math.Pi * c.radius * c.radius) * c.height
 }
 
 func main() {
-	r1 := RectangularParallelepiped{10.5, 20.2,20}
+	r1 := RectangularParallelepiped{10.5, 20.2, 20}
 	c1 := Cylinder{10, 10}
-	r2 := RectangularParallelepiped{4,10,23}
-	c2 := Cylinder{4.2,15.6}
+	r2 := RectangularParallelepiped{4, 10, 23}
+	c2 := Cylinder{4.2, 15.6}
 
-	printMeasure(c1,c2 ,r1, r2)
+	printMeasure(c1, c2, r1, r2)
 }
 
 func printMeasure(m ...geometry) { //인터페이스를 가변 인자로 하는 함수
 	for _, val := range m { //가변 인자 함수의 값은 슬라이스형
-		fmt.Printf("%.2f, ",val.area()) //인터페이스의 메소드 호출
-		fmt.Printf("%.2f\n",val.volume()) //인터페이스의 메소드 호출
+		fmt.Printf("%.2f, ", val.area())   //인터페이스의 메소드 호출
+		fmt.Printf("%.2f\n", val.volume()) //인터페이스의 메소드 호출
 	}
 }
--- golang/main.go
+++ golang/main.go
@@ -1,61 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type CirQueue struct {
-	array       [10]int
-	front, rear int
-}
+func changeValue(num *int) {
 
-func (c *CirQueue) isEmpty() bool {
-	return c.front == c.rear
-}
-
-func (c *CirQueue) isFull() bool {
-
-	return ((c.rear + 1) % cap(c.array)) == c.front
-
-}
-
-// 큐에서 데이터를 삽입한다.
-func (c *CirQueue) enqueue(data int) {
-	if c.isFull() {
-		fmt.Println("Q is full")
-		return
-	}
-
-	fmt.Println(c.rear, data)
-	// 원형 큐이기 때문에 SIZE보다 크면 다시 처음부터 원소가 삽입되어야하므로 모퓰러 연산이 들어간다.
-	c.rear = (c.rear + 1) % cap(c.array)
-
-	c.array[c.rear] = data
-}
-
-// 큐에서 데이터를 꺼내온다.
-func (c *CirQueue) dequeue() int {
-	if c.isEmpty() {
-		fmt.Println("Q is Empty")
-		return 0
-	}
-
-	// 원형 큐이기 때문에 SIZE보다 크면 다시 처음으로 돌아가 원소를 꺼내와야 되므로 모퓰러 연산이 들어간다.
-	c.front = (c.front + 1) % cap(c.array)
-
-	return c.array[c.front]
+	fmt.Println(num)
+	*num = *num + 1
 }
 
 func main() {
+	num := 1
+	fmt.Println(num) // 출력: 1
+	fmt.Println(&num)
 
-	var q CirQueue
-
-	q.front, q.rear = 9, 9
-
-	for i := 1; i < 12; i++ {
-		q.enqueue(i)
-	}
-
+	changeValue(&num)
+	fmt.Println(num) // 출력: 2
 }
-
-/**
-* 참고 : https://reakwon.tistory.com/30
-**/
